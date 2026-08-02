b7328f976f4df6b877e26bd677f697a5140996f8
Andrii S <semko.andrey.i@gmail.com>
2026-08-02 17:23:36 +0300

    chore: add commitlint configuration and update mdurl resource version in Homebrew formula

--- Formula/hoko.rb
+++ Formula/hoko.rb
@@ -29,7 +29,7 @@ class Hoko < Formula
   end
 
   resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.4.tar.gz"
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
     sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
   end
 
